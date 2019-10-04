6aa77bf03d2bb1aeed6550e5f3b26d1e42fb0fde
Benjamin McLean <mclean.ben@gmail.com>
2019-10-03 21:30:47 -0500

    Added unit test

--- Model/IO/VoxIO.cs
+++ Model/IO/VoxIO.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WarpWriter.Model.Fetch;
 
 namespace WarpWriter.Model.IO
 {
-    class VoxIO
+    public static class VoxIO
     {
         /// <summary>
         /// MagicaVoxel's default palette; used when no palette is specified.
---
+++ WarpWriterTest/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WarpWriter.Model.Fetch;
+using WarpWriter.Model.IO;
+using WarpWriter.View;
+using WarpWriter.View.Color;
+using WarpWriter.View.Render;
+
+namespace WarpWriterTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ArrayModel model;
+            uint[] palette;
+            using (FileStream file = new FileStream(@"..\..\..\Artillery.vox", FileMode.Open))
+                VoxIO.ReadVox(file, out model, out palette);
+
+            ByteArrayRenderer renderer = new ByteArrayRenderer()
+            {
+                Width = PixelCubeDraw.IsoWidth(model),
+                Height = PixelCubeDraw.IsoHeight(model),
+                Color = new FlatVoxelColor()
+                {
+                    Palette = palette,
+                }
+            }.PixelCubeIso(model);
+        }
+    }
+}
